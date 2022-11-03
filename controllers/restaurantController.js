c4f4377a7ec1f751efb1f99796ba8153bfb1536f
nellie789 <62j9@fresnou.org>
2022-11-03 11:36:47 -0700

    finished 4.3.2

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,11 +1,66 @@
-module.exports.viewAll = function(req, res, next) {
+const {Restaurant} = require('../models')
+const categories = ['Italian', 'Fast Food'];
+module.exports.viewAll = async function(req, res, next){
+    const restaurants = await Restaurant.findAll();
+    res.render('index', {restaurants});
+}
+
+
+module.exports.renderEditForm = async function(req, res, next) {
+    const restaurant = await Restaurant.findByPk(
+        req.params.id
+    );
+    res.render('edit', {restaurant, categories});
+}
+
+module.exports.updateRestaurant = async function(req,res) {
+    await Restaurant.update(
+        {
+            name: req.body.name,
+            category: req.body.category,
+            rating: req.body.rating,
+            image: req.body.image,
+            description: req.body.description
+        },
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
+}
+
+module.exports.deleteRestaurant = async function(req,res) {
+    await Restaurant.destroy(
+    {
+        where:
+        {
+            id: req.params.id
+        }
+    });
+    res.redirect('/');
+}
+
+module.exports.renderAddForm = function(req, res){
     const restaurant = {
-        id: 1,
-        name: 'Pizza Hut',
-        image: 'https://dynl.mktgcdn.com/p/d9AXTJEWMZ156q11dLLVRHsmufNu0K-ng4JYb_4WwRI/619x464.jpg',
-        rating: 2,
-        category: 'Italian',
-        description: 'asldkfjasldf jadjfasd fasdf asdf as asldkfjasldf jadjfasd fasdf asdf as asldkfjasldf jadjfasd fasdf asdf as asldkfjasldf jadjfasd fasdf asdf as asldkfjasldf jadjfasd fasdf asdf as asldkfjasldf jadjfasd fasdf asdf as asldkfjasldf jadjfasd fasdf asdf as asldkfjasldf jadjfasd fasdf asdf as'
+        name: "",
+        description: "",
+        rating: 1,
+        image: "",
+        category: categories[0],
     };
-    res.render('index', {restaurant});
+    res.render('add', {restaurant, categories });
+}
+
+module.exports.addRestaurant = async function(req, res) {
+    await Restaurant.create(
+        {
+            name: req.body.name,
+            category: req.body.category,
+            rating: req.body.rating,
+            image: req.body.image,
+            description: req.body.description
+        });
+    res.redirect('/');
 }
